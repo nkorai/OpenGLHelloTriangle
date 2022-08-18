635cfabaca06d5a89ffc1b8fc2b87eba06a74118
Naush <nausherwan.korai@gmail.com>
2022-08-18 09:13:17 -0400

    Code refactoring and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OpenGLHelloTriangle
+
+Experimenting around with OpenGL Hello Triangle.
+Documented some notes here: https://nkorai.com/opengl-hello-triangle/
--- main.cpp
+++ main.cpp
@@ -16,6 +16,47 @@ string readFileIntoString(const string& path) {
     return str;
 }
 
+GLuint generateVao(const float points[]) {
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    return vao;
+};
+
+GLuint generateShaderProgram(const string vertexShaderPath, const string fragmentShaderPath) {
+    // Vertex shader
+    string vertShaderString = readFileIntoString(vertexShaderPath);
+    const char* vertex_shader = vertShaderString.c_str();
+
+    // Fragment shader 
+    string fragmentShaderString = readFileIntoString(fragmentShaderPath);
+    const char* fragment_shader = fragmentShaderString.c_str();
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, NULL);
+    glCompileShader(fs);
+
+    // Program
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, fs);
+    glAttachShader(shader_program, vs);
+    glLinkProgram(shader_program);
+
+    return shader_program;
+}
+
 int main() {
     // Start GL context and OS window using the GLFW helper library
     if (!glfwInit()) {
@@ -58,83 +99,33 @@ int main() {
         -0.5f, -0.5f, 0.0f
     };
 
-    GLuint vbo1 = 0;
-    glGenBuffers(1, &vbo1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points1, GL_STATIC_DRAW);
-
-    GLuint vao1 = 0;
-    glGenVertexArrays(1, &vao1);
-    glBindVertexArray(vao1);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
     float points2[] = {
         -0.5f, 0.5f, 0.0f,
         0.5f, 0.5f, 0.0f,
         0.5f, -0.5f, 0.0f
     };
 
-    GLuint vbo2 = 0;
-    glGenBuffers(1, &vbo2);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points2, GL_STATIC_DRAW);
+    GLuint vao1 = generateVao(points1);
+    GLuint vao2 = generateVao(points2);
 
-    GLuint vao2 = 0;
-    glGenVertexArrays(1, &vao2);
-    glBindVertexArray(vao2);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-    // Vertex shader
-    string vertShaderString = readFileIntoString("./shaders/test_vs.glsl");
-    const char* vertex_shader = vertShaderString.c_str();
-
-    // Fragment shader yellow
-    string fragmentShaderYellowString = readFileIntoString("./shaders/test_fs_yellow.glsl");
-    const char* fragment_shader_yellow = fragmentShaderYellowString.c_str();
-
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, NULL);
-    glCompileShader(vs);
-
-    GLuint fs_yellow = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs_yellow, 1, &fragment_shader_yellow, NULL);
-    glCompileShader(fs_yellow);
-
-    GLuint shader_program_yellow = glCreateProgram();
-    glAttachShader(shader_program_yellow, fs_yellow);
-    glAttachShader(shader_program_yellow, vs);
-    glLinkProgram(shader_program_yellow);
-
-    // Fragment shader pink
-    string fragmentShaderPinkString = readFileIntoString("./shaders/test_fs_pink.glsl");
-    const char* fragment_shader_pink = fragmentShaderPinkString.c_str();
-
-    GLuint fs_pink = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs_pink, 1, &fragment_shader_pink, NULL);
-    glCompileShader(fs_pink);
-
-    GLuint shader_program_pink = glCreateProgram();
-    glAttachShader(shader_program_pink, fs_pink);
-    glAttachShader(shader_program_pink, vs);
-    glLinkProgram(shader_program_pink);
+    // Shaders
+    GLuint shader_program_yellow = generateShaderProgram("./shaders/test_vs.glsl", "./shaders/test_fs_yellow.glsl");
+    GLuint shader_program_pink = generateShaderProgram("./shaders/test_vs.glsl", "./shaders/test_fs_pink.glsl");
 
     glClearColor(0.0f, 0.9f, 0.9f, 1.0f);
+
     while (!glfwWindowShouldClose(window)) {
         // Wipe the drawing surface clear
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //// First
+        //// First VAO drawing
         glUseProgram(shader_program_yellow);
         glBindVertexArray(vao1);
 
         // Draw points 0-3 from the currently bound VAO with the current in-use shader
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        //// Second
+        //// Second VAO drawing
         glUseProgram(shader_program_pink);
         glBindVertexArray(vao2);
 
